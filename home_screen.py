01702f7e74e93c318f9a39d6468ba77d712cced4
JENVSensor <cgy.envsensor@gmail.com>
2023-12-11 17:30:34 +0900

    #15 PM1.0/PM2.5/PM10 수정

--- home_screen.py
+++ home_screen.py
@@ -950,27 +950,25 @@ class Home(ttk.Frame):
                 else:
                         self.CO2_label.config(text=self.CO2)
                 
-                # self.controller.PM1로부터 원래의 PM1.0 값을 찾음
-                original_PM1 = (self.controller.PM1 - 5.1584) / 0.554
 
                 # 새로운 계산식을 적용
-                self.PM1 = 0.5791 * original_PM1 + 4.9836
+                self.PM1 = (0.554  * original_PM1) + 5.1584
                 #self.PM1 = self.controller.PM1
                 if self.PM1 < 0:
                         self.PM1_label.config(text='...')        
                 else:
                         self.PM1_label.config(text=int(self.PM1))
                 
-                #PM25_1 = self.controller.PM25 - self.PM1  # PM2.5에서 PM1을 뺌
-                #self.PM25 = self.controller.PM25 - self.PM1
-                self.PM25 = self.controller.PM25
+                PM25_1 = self.controller.PM25 - self.PM1  # PM2.5에서 PM1을 뺌
+                self.PM25 = self.controller.PM25 - self.PM1
+                #self.PM25 = self.controller.PM25
                 if self.PM25 < 0:
                         self.PM25_label.config(text='...')        
                 else:
                         self.PM25_label.config(text=int(self.PM25))
                 
-                #self.PM10 = ((self.controller.PM10  -PM25_1) - self.PM1)
-                self.PM10 = self.controller.PM10
+                self.PM10 = ((self.controller.PM10  -PM25_1) - self.PM1)
+                #self.PM10 = self.controller.PM10
                 if self.PM10 < 0:
                         self.PM10_label.config(text='...')        
                 else:
