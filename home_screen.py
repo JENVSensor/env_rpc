4d4306afc7d748cc73f4f6ca55a2d37bdb165d39
JENVSensor <cgy.envsensor@gmail.com>
2024-02-02 17:37:30 +0900

    Update home_screen.py

--- home_screen.py
+++ home_screen.py
@@ -1084,7 +1084,7 @@ class Home(ttk.Frame):
 
                 if self.controller.device_number == 1:
                         self.PM1 = self.PM1 * 0.63
-                        self.PM25 = (self.PM25 - self.PM1 * 1.69)
+                        self.PM25 = (self.PM25 - self.PM1 * 1.67)
                         self.PM10 = self.PM10  - (self.PM25 * 1.39)
 
                 if self.controller.device_number == 3:
