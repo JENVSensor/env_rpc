fc46c57d8f330f816998b3f386286f55bb7b1234
JENVSensor <cgy.envsensor@gmail.com>
2024-02-05 15:23:05 +0900

    Update home_screen.py

--- home_screen.py
+++ home_screen.py
@@ -1197,10 +1197,10 @@ class Home(ttk.Frame):
                         self.PM10 = self.PM10  - (self.PM25 * 1.39)
 
                 #32번일 경우  y=(pm10 - pm2.5) - pm1.0
-                if self.controller.device_number == 32:
-                        self.PM1 = self.PM1
-                        self.PM25 = (self.PM25 - self.PM1 * 0.5)
-                        self.PM10 = self.PM10  - (self.PM25 * 2) / 7 - (self.PM25 / 5)
+                # if self.controller.device_number == 32:
+                #         self.PM1 = self.PM1
+                #         self.PM25 = (self.PM25 - self.PM1 * 0.5)
+                #         self.PM10 = self.PM10  - (self.PM25 * 2) / 7 - (self.PM25 / 5)
 
                 if self.controller.device_number == 33:
                         self.PM25 = (self.PM25 - self.PM1 * 0.4 )                     
