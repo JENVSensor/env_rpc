ce780b19547c29325226e4160d995c681623abfd
JENVSensor <148833062+JENVSensor@users.noreply.github.com>
2024-02-14 09:56:10 +0900

    Update home_screen.py

--- home_screen.py
+++ home_screen.py
@@ -1077,10 +1077,10 @@ class Home(ttk.Frame):
                 #self.PM10 = ((self.controller.PM10  -PM25_1) - self.PM1)
                 self.PM10 = self.controller.PM10
 
-                if self.controller.device_number == 0:
-                        self.PM1 = self.PM1 * 0.67
-                        self.PM25 = (self.PM25 - self.PM1 * 1.52)
-                        self.PM10 = self.PM10  - (self.PM25 * 1.33)
+                # if self.controller.device_number == 0:
+                #         self.PM1 = self.PM1 * 0.67
+                #         self.PM25 = (self.PM25 - self.PM1 * 1.52)
+                #         self.PM10 = self.PM10  - (self.PM25 * 1.33)
                         
                 if self.controller.device_number == 1:
                         self.PM1 = self.PM1 * 0.63
